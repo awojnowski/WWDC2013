6327e5d9ab329dbb3e9c41ed8b6b2bff99ec03fc
Aaron Wojnowski <aaronwojnowski@gmail.com>
2013-04-29 12:19:23 -0500

    Add Icon file.

---
+++ WWDC2013/WWImageView.h
@@ -0,0 +1,16 @@
+//
+//  WWImageView.h
+//  WWDC2013
+//
+//  Created by Aaron Wojnowski on 2013-04-29.
+//  Copyright (c) 2013 Aaron Wojnowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WWImageView : UIImageView
+
+// used for storing preloaded image names
+@property (nonatomic, strong) NSString *imageName;
+
+@end
--- WWDC2013/WWImageView.m
+++ WWDC2013/WWImageView.m
@@ -1,13 +1,15 @@
 //
-//  WWBlurredImageView.h
+//  WWImageView.m
 //  WWDC2013
 //
-//  Created by Aaron Wojnowski on 2013-04-25.
+//  Created by Aaron Wojnowski on 2013-04-29.
 //  Copyright (c) 2013 Aaron Wojnowski. All rights reserved.
 //
 
 #import "WWImageView.h"
 
-@interface WWBlurredImageView : WWImageView
+@implementation WWImageView
+
+
 
 @end
